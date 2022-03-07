a0ab1803da94066e2d90c4bb485811224009c764
simidawalang <simidawalang@gmail.com>
2022-03-07 14:04:48 +0100

    started conditional rendering depending on MetaMask presence

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,38 +2,58 @@ import React from "react";
 import Link from "next/link";
 import { Button, Card } from "semantic-ui-react";
 import factory from "../ethereum/factory";
+import web3 from "../ethereum/web3";
 
-const CampaignIndex = ({ campaigns }) => {
-  const renderCampaigns = () => {
-    const items = campaigns.map((address) => {
-      return {
-        header: address,
-        description: <Link href={`/campaigns/${address}`}>View Charity</Link>,
-        fluid: true,
-      };
-    });
-
-    return <Card.Group items={items} />;
+const Home = ({ accounts }) => {
+  const connectWallet = async () => {
+    try {
+      console.log(accounts[0]);
+    } catch (e) {
+      console.log(e.message);
+    }
   };
   return (
-    <div>
-      <h3>Open Campaigns</h3>
-      <Link href="/campaigns/new">
-        <Button
-          floated="right"
-          content="Create Charity"
-          icon="add circle"
-          primary
-        />
-      </Link>
-      {renderCampaigns()}
+    <div className="home-page">
+      <h3 className="home-header">Welcome to CharityHub!</h3>
+      <p>
+        CharityHub is a decentralized charity platform which ensures that
+        donations get to their intended destinations and causes. As a donator,
+        you get to see withdrawal requests and from the charity organizers. You
+        can even make your own charity.
+      </p>
+      <div className="home-page-actions">
+        {web3.currentProvider.isMetaMask ? (
+          <>
+            <Link href="/campaigns/new">
+              <Button content="Create Charity" color="purple" />
+            </Link>
+          </>
+        ) : (
+          <p>
+            Hmmm... Seems like you don't have MetaMask installed on ypur
+            browser. Please, install{" "}
+            <a
+              href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=en"
+              className="warning-text"
+              rel="noreferrer"
+            >
+              <span>here</span>
+            </a>{" "}
+            to donate to or create charities.
+          </p>
+        )}
+        <Link href="/campaigns">
+          <Button content="View Charities" primary />
+        </Link>
+      </div>
     </div>
   );
 };
 
-CampaignIndex.getInitialProps = async () => {
+Home.getInitialProps = async () => {
   const campaigns = await factory.methods.getDeployedCampaigns().call();
-  return { campaigns };
+  const accounts = await web3.eth.getAccounts();
+  return { campaigns, accounts };
 };
 
-export default CampaignIndex;
+export default Home;
